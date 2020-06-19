fc20489a76cf78fc43ee2c5e2d6274a43af5fd33
xhanti-fatyela <xmanmrcool@gmail.com>
2020-06-19 11:02:19 +0200

    added something to calculate bill test

--- dom-tests/calculate-bill-test.js
+++ dom-tests/calculate-bill-test.js
@@ -12,5 +12,17 @@ describe("the calculate bill factory function", function () {
 
      });
 
+     it("should be able to give total when 'call' is entered more than once", function () {
 
+        assert.equal(8.25, totalPhoneBill("call,call,call"))
+
+   });
+
+
+
+   it("should be able to give total when 'call,sms,call,sms' is entered more than once", function () {
+
+    assert.equal(6.80, totalPhoneBill("call,sms,call,sms"))
+
+});
 });
